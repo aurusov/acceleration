732ee1689b33af30c7111832de0c012377f7685b
Andrey Urusov <drobus@gmail.com>
2012-01-15 12:54:22 +0000

    - учитывается тормозной путь
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root/acceleration@6265 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 typedef double Speed;
 typedef double Length;
@@ -8,20 +9,52 @@ typedef double TimeStep;
 static const TimeStep     dT   = 1.0;
 static const Length       Xs   = 1000.0;
 static const Speed        Vmax = 30.5;
+static const Speed        Vmin = 0.0;
 static const Acceleration Amax = 6.0;
 
 static Length       Xm = 0.0;
 static Speed        Vm = 0.0;
 static Acceleration Am = 0.0;
 
+//! “ормозной путь
+//! param  A - ускорение, неизмен€ема€ характеристика автомобил€, зависит от его мощности
+//! param  V - текуща€ скросроть автомобил€
+//! result     величина тормозного пути
+Length brakingDistance(Acceleration A, Speed V)
+{
+	return (V * V) / (2 * A);
+}
+
 Acceleration AccelerationNext(Length Li, Speed Vi, Acceleration Ai)
 {
-	//! –азгон с нул€ без учЄта рассто€ни€ до стены
-	if (Vi == 0.0 && Ai == 0.0)
-		return Amax;
+	//if (Li < 0)
+	//	return -Amax;
 
-	double dT = 100;
-	Acceleration Anext = (2 * Li - 2 * Vi * dT) / dT / dT;
+	Length Lb = brakingDistance(Amax, Vi);
+
+	Acceleration Anext;
+
+	Length delta        = Li - Lb;
+	Length deltaAbs     = fabs(delta);
+	Length minPrecision = Vi * dT;
+
+	if (delta < minPrecision)
+	{
+		Anext = -Amax;
+	}
+	else if (delta > 2 * minPrecision)
+	{
+		Anext = Amax;
+	}
+	else
+	{
+		Anext = Ai;
+	}
+
+	if (Li > 0.0 && Li < 3.0)
+	{
+		Anext = -Amax;
+	}
 
 	if (Anext > Amax)
 		Anext = Amax;
@@ -38,7 +71,19 @@ Speed SpeedNext(Length Li, Speed Vi)
 
 	Speed Vnext = Am * dT + Vi;
 	if (Vnext > Vmax)
+	{
 		Vnext = Vmax;
+		Am    = 0.0;
+	}
+	else if (Vnext < -Vmin)
+	{
+		Vnext = -Vmin;
+		Am    = 0.0;
+	}
+	else if (fabs(Vnext) < 0.1)
+	{
+		Vnext = 0.0;
+	}
 
 	return Vnext;
 }
@@ -54,7 +99,7 @@ Length LengthNext(Length Li)
 void main()
 {
 	Length delta = Xs - Xm;
-	while (Vm >= 0.0)
+	while (fabs(Vm) > 0.0 || fabs(delta) > 10.0)
 	{
 		Xm += LengthNext(delta);
 		delta = Xs - Xm;
