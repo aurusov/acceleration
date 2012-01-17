7e3c04e3dd1dae187a3b6fd7e3bd303aa08d0dba
Andrey Urusov <drobus@gmail.com>
2012-01-17 20:28:52 +0000

    - более правильная остановка
    git-svn-id: svn://rdo.rk9.bmstu.ru/var/svn/root/acceleration@6273 23f926e6-80f1-49ca-85a0-60d4a27bb3ac

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ typedef double Length;
 typedef double Acceleration;
 typedef double TimeStep;
 
-static const TimeStep     dT   = 0.5;
+static const TimeStep     dT   = 1.0;
 static const Length       Xs   = 1000.0;
 static const Speed        Vmax = 30.5;
 static const Speed        Vmin = 0.0;
@@ -17,20 +17,16 @@ static Speed        Vm = 0.0;
 static Acceleration Am = 0.0;
 
 //! Тормозной путь
-//! param  A - ускорение, неизменяемая характеристика автомобиля, зависит от его мощности
 //! param  V - текущая скросроть автомобиля
 //! result     величина тормозного пути
-Length brakingDistance(Acceleration A, Speed V)
+Length brakingDistance(Speed V)
 {
-	return (V * V) / (2 * A);
+	return (V * V) / (2 * Amax);
 }
 
 Acceleration AccelerationNext(Length Li, Speed Vi, Acceleration Ai)
 {
-	//if (Li < 0)
-	//	return -Amax;
-
-	Length Lb = brakingDistance(Amax, Vi);
+	Length Lb = brakingDistance(Vi);
 
 	Acceleration Anext;
 
@@ -38,22 +34,17 @@ Acceleration AccelerationNext(Length Li, Speed Vi, Acceleration Ai)
 	Length deltaAbs     = fabs(delta);
 	Length minPrecision = Vi * dT;
 
-	if (delta < minPrecision)
+	if (delta <= 0.0)
 	{
 		Anext = -Amax;
 	}
-	else if (delta > 2 * minPrecision)
+	else if (delta > 0.0)
 	{
 		Anext = Amax;
 	}
 	else
 	{
 		Anext = Ai;
-	}
-
-	if (Li > 0.0 && Li < 3.0)
-	{
-		Anext = -Amax;
 	}
 
 	if (Anext > Amax)
@@ -65,20 +56,16 @@ Acceleration AccelerationNext(Length Li, Speed Vi, Acceleration Ai)
 	return Anext;
 }
 
-Speed SpeedNext(Length Li, Speed Vi)
+Speed SpeedNext(Length Li, Speed Vi, Acceleration Ai)
 {
-	Am = AccelerationNext(Li, Vi, Am);
-
-	Speed Vnext = Am * dT + Vi;
+	Speed Vnext = Ai * dT + Vi;
 	if (Vnext > Vmax)
 	{
 		Vnext = Vmax;
-		Am    = 0.0;
 	}
 	else if (Vnext < -Vmin)
 	{
 		Vnext = -Vmin;
-		Am    = 0.0;
 	}
 	else if (fabs(Vnext) < 0.1)
 	{
@@ -88,27 +75,41 @@ Speed SpeedNext(Length Li, Speed Vi)
 	return Vnext;
 }
 
-Length LengthNext(Length Li)
+Length LengthNext(Length Li, Speed Vi, Acceleration Ai)
 {
-	Vm = SpeedNext(Li, Vm);
-
-	Length Lnext = Vm * dT + Am * dT * dT / 2;
+	Length Lnext = Vi * dT + fabs(Ai) * dT * dT / 2;
 	return Lnext;
 }
 
 void main()
 {
 	Length delta = Xs - Xm;
-	while (fabs(Vm) > 0.0 || fabs(delta) > 10.0)
+	while (true)
 	{
-		Xm += LengthNext(delta);
-		delta = Xs - Xm;
+		Acceleration Anext = AccelerationNext(delta, Vm, Am);
+		Speed        Vnext = SpeedNext       (delta, Vm, Anext);
+		Length       Lnext = delta - LengthNext(delta, Vnext, Anext);
+
+		Am = AccelerationNext(Lnext, Vnext, Anext);
+		Vm = SpeedNext       (delta, Vm, Am);
+		if (Vm >= Vmax)
+		{
+			Am = 0.0;
+		}
+		else if (Vm <= -Vmin)
+		{
+			Am = 0.0;
+		}
+		Xm += LengthNext(delta, Vm, Am);
 
 		std::cout << "Am = " << Am << "   "
 		          << "Vm = " << Vm << "   "
 		          << "Xm = " << Xm << "   "
 		          << std::endl;
-	}
 
-	int i = 1;
+		delta = Xs - Xm;
+
+		if (fabs(Vm) == 0.0)
+			break;
+	}
 }
